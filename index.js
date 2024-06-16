3e76cf292186c4a15f2908c1fbb391be5fbc20c1
Jae Jeong <jeongjj29@gmail.com>
2024-06-16 04:04:15 -0400

    adds object functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Elmo",
+    streetAddress: "123 Sesame Street",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newobj =  {...obj};
+    newobj[key] = value;
+    return newobj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newobj = {...obj};
+    delete newobj[key];
+    return newobj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
